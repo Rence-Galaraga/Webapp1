120d361a50af24a2c48ac6eb16c4d8ca67f1c47b
renlagalaraga <razor64102@gmail.com>
2021-02-04 14:21:53 +0800

    New files for pa-2

---
+++ app.js
@@ -0,0 +1,3 @@
+const mymodule = require('./mymodule');
+mymodule.clickMe(mymodule.fname[0]);
+console.log(mymodule);
---
+++ mymodule.js
@@ -0,0 +1,5 @@
+const fname = ["Rence","Pan","Christer","Tan"];
+const clickMe = function(fn = null){
+    console.log("Hello " + fn);
+}
+module.exports = {fname , clickMe};
